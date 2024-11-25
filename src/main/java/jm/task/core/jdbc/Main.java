30a135f9e5e3b58be67f377a4757cd45443dc4d1
RostikSlavik <sergeybelg1@gmail.com>
2024-11-25 15:50:51 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,13 +7,18 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // реализуйте алгоритм здесь
         UserService userService = new UserServiceImpl();
+
         userService.createUsersTable();
+
         userService.saveUser("John", "Doe", (byte) 25);
         userService.saveUser("Jane", "Smith", (byte) 30);
+        userService.saveUser("Paula", "Johnson", (byte) 20);
+        userService.saveUser("Stephen", "Bowen", (byte) 48);
 
-        userService.getAllUsers().forEach(user ->
-                System.out.println(user.getName() + " " + user.getLastName()));
+        userService.getAllUsers().forEach(user -> System.out.println(user));
 
         userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
